c0e2e5290873a3dddeb129d25a26891e9f3b35a5
Jack Coble <jack@coble.uk>
2020-11-10 20:52:35 +0000

    Start of content for Home page

--- src/views/Tab1.vue
+++ src/views/Tab1.vue
@@ -1,28 +1,75 @@
 <template>
   <ion-page>
     <ion-header>
-      <ion-toolbar>
-        <ion-title>Tab 1</ion-title>
+      <ion-toolbar class="ion-text-center">
+        <ion-title>Feirm Account</ion-title>
       </ion-toolbar>
     </ion-header>
     <ion-content :fullscreen="true">
       <ion-header collapse="condense">
         <ion-toolbar>
-          <ion-title size="large">Tab 1</ion-title>
+          <ion-title size="large">Feirm Account</ion-title>
         </ion-toolbar>
       </ion-header>
-    
-      <ExploreContainer name="Tab 1 page" />
+      <!-- Content goes in here -->
+      <ion-card>
+        <ion-card-content>
+          <p class="ion-text-center">
+            <b>Shortcuts</b>
+          </p>
+          <ion-grid>
+            <ion-row class="ion-text-center">
+              <ion-col>
+                <ion-button router-link="/">
+                  <ion-icon slot="icon-only" :icon="qrCodeOutline" />
+                </ion-button>
+              </ion-col>
+            </ion-row>
+          </ion-grid>
+        </ion-card-content>
+      </ion-card>
     </ion-content>
   </ion-page>
 </template>
 
 <script lang="ts">
-import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
-import ExploreContainer from '@/components/ExploreContainer.vue';
+import {
+  IonPage,
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+  IonCard,
+  IonCardContent,
+  IonGrid,
+  IonRow,
+  IonIcon,
+  IonButton
+} from "@ionic/vue";
 
-export default  {
-  name: 'Tab1',
-  components: { ExploreContainer, IonHeader, IonToolbar, IonTitle, IonContent, IonPage }
-}
+import {
+  qrCodeOutline
+} from "ionicons/icons"
+
+export default {
+  name: "Tab1",
+  components: {
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonContent,
+    IonPage,
+    IonCard,
+    IonCardContent,
+    IonGrid,
+    IonRow,
+    IonIcon,
+    IonButton
+  },
+  setup() {
+    return {
+      qrCodeOutline
+    }
+  }
+};
 </script>
