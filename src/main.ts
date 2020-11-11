c877732d868be3194bf2ee1fa0086d76a582bad7
Jack Coble <jack@coble.uk>
2020-11-11 13:46:26 +0000

    Initial support for PWA

--- src/main.ts
+++ src/main.ts
@@ -23,6 +23,9 @@ import '@ionic/vue/css/display.css';
 /* Theme variables */
 import './theme/variables.css';
 
+/* PWA Service Worker */
+import './registerServiceWorker';
+
 const app = createApp(App)
   .use(IonicVue)
   .use(router);
