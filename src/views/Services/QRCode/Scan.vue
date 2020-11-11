e9df67d5f07d5da659f453909f2cd5cf3f04072e
Jack Coble <jack@coble.uk>
2020-11-11 14:43:30 +0000

    Initial structure of QR Scan page

---
+++ src/views/Services/QRCode/Scan.vue
@@ -0,0 +1,38 @@
+<template>
+  <ion-page>
+    <ion-header>
+      <ion-toolbar>
+        <ion-buttons slot="start">
+          <ion-back-button default-href="/"></ion-back-button>
+        </ion-buttons>
+      </ion-toolbar>
+    </ion-header>
+    <ion-content :fullscreen="true" class="ion-padding">
+        Hello
+    </ion-content>
+  </ion-page>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import {
+    IonPage,
+    IonHeader,
+    IonToolbar,
+    IonButtons,
+    IonBackButton,
+    IonContent
+} from "@ionic/vue"
+
+export default defineComponent({
+    name: 'Scan',
+    components: {
+        IonPage,
+        IonHeader,
+        IonToolbar,
+        IonButtons,
+        IonBackButton,
+        IonContent
+    }
+})
+</script>
