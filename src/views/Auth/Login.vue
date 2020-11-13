458052e82dda1cc56e327756a72609fab7016fb2
Jack Coble <jack@coble.uk>
2020-11-13 13:21:25 +0000

    Finish design for the Login page

--- src/views/Auth/Login.vue
+++ src/views/Auth/Login.vue
@@ -1,32 +1,65 @@
 <template>
   <ion-page>
     <ion-content :fullscreen="true" class="ion-padding">
-      <ion-title class="ion-text-center">Login to your Feirm Account</ion-title>
+      <ion-grid>
+        <ion-row>
+          <ion-col>
+            <ion-img src="/assets/logo.png"></ion-img>
+            <ion-text class="ion-text-center">
+              <h1>Hello there.</h1>
+              <p>Please Login or Register to continue.</p>
+            </ion-text>
+            <ion-card>
+              <ion-card-content>
+                <form @submit.prevent>
+                  <ion-item>
+                    <ion-label position="floating">Username</ion-label>
+                    <ion-input></ion-input>
+                  </ion-item>
+                  <ion-item>
+                    <label position="floating">Password</label>
+                    <ion-input></ion-input>
+                  </ion-item>
+                  <br />
+                  <ion-button expand="block">Login</ion-button>
+                  <ion-button expand="block" color="dark">Register</ion-button>
+                </form>
+              </ion-card-content>
+            </ion-card>
+          </ion-col>
+        </ion-row>
+      </ion-grid>
     </ion-content>
   </ion-page>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
-import { IonPage, IonContent, IonTitle } from "@ionic/vue";
+import { IonPage, IonContent } from "@ionic/vue";
 
 export default defineComponent({
   name: "Login",
   components: {
     IonPage,
     IonContent,
-    IonTitle,
   },
 });
 </script>
 
-<style>
-body {
-  background: linear-gradient(rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.8)),
-    url("https://source.unsplash.com/daily?water") no-repeat center/cover fixed;
+<style scoped>
+ion-img {
+  margin: 0 auto;
+  width: 100px;
 }
 
-ion-content {
-  --background: none;
+ion-grid{
+    height: 100%;
+}
+
+ion-row{
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 100%;
 }
 </style>
