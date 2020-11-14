f8072184daf5f60bf257e6e81b02e5005e154bfd
Jack Coble <jack@coble.uk>
2020-11-14 21:35:19 +0000

    Cleanup the service worker

--- src/registerServiceWorker.ts
+++ src/registerServiceWorker.ts
@@ -21,7 +21,6 @@ if (process.env.NODE_ENV === 'production') {
       console.log('New content is downloading.')
     },
     updated () {
-      console.log('New content is available; please refresh.')
       router.push({path: '/update'})
     },
     offline () {
