f49aa36ef94245ce6ce3e644ed3cf746ef9ce491
Jack Coble <jack@coble.uk>
2020-11-21 18:46:00 +0000

    Wallet page redesign

--- src/views/Wallet/Overview.vue
+++ src/views/Wallet/Overview.vue
@@ -1,19 +1,24 @@
 <template>
   <ion-page>
     <ion-toolbar class="ion-text-center">
-      <ion-title>Wallet</ion-title>
+      <ion-title>Wallets</ion-title>
     </ion-toolbar>
     <ion-content :fullscreen="true" class="ion-padding">
       <ion-card>
         <ion-card-header color="primary">
-          <ion-card-title size="small">Main Wallet</ion-card-title>
+          <ion-card-title>Feirm Wallets</ion-card-title>
         </ion-card-header>
         <ion-card-content>
-            <ion-list>
-                <ion-item>
-                    <ion-label>Feirm (XFE)</ion-label>
-                </ion-item>
-            </ion-list>
+          <ion-list>
+            <ion-item lines="none" button="true">
+              <ion-icon
+                :icon="walletOutline"
+                size="large"
+                slot="start"
+              ></ion-icon>
+              <ion-label>Personal</ion-label>
+            </ion-item>
+          </ion-list>
         </ion-card-content>
       </ion-card>
     </ion-content>
@@ -33,8 +38,9 @@ import {
   IonCardContent,
   IonList,
   IonItem,
-  IonLabel
+  IonLabel,
 } from "@ionic/vue";
+import { walletOutline } from "ionicons/icons";
 
 export default defineComponent({
   name: "WalletOverview",
@@ -49,23 +55,24 @@ export default defineComponent({
     IonCardContent,
     IonList,
     IonItem,
-    IonLabel
-  }
+    IonLabel,
+  },
+  setup() {
+    return {
+      walletOutline,
+    };
+  },
 });
 </script>
 
 <style scoped>
 ion-card-header {
-    padding-top: 0.4em;
-    padding-bottom: 0.4em;
+  padding-top: 0.4em;
+  padding-bottom: 0.4em;
 }
 
 ion-card-title {
-    font-size: 1rem;
-    font-weight: bold;
-}
-
-.icon-right {
-    float: right;
+  font-size: 1rem;
+  font-weight: bold;
 }
 </style>
