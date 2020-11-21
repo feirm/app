0555711b3796e5698a1293f189600d1899233d5f
Jack Coble <jack@coble.uk>
2020-11-21 18:04:37 +0000

    Start of wallet design page

---
+++ src/views/Wallet/Overview.vue
@@ -0,0 +1,71 @@
+<template>
+  <ion-page>
+    <ion-toolbar class="ion-text-center">
+      <ion-title>Wallet</ion-title>
+    </ion-toolbar>
+    <ion-content :fullscreen="true" class="ion-padding">
+      <ion-card>
+        <ion-card-header color="primary">
+          <ion-card-title size="small">Main Wallet</ion-card-title>
+        </ion-card-header>
+        <ion-card-content>
+            <ion-list>
+                <ion-item>
+                    <ion-label>Feirm (XFE)</ion-label>
+                </ion-item>
+            </ion-list>
+        </ion-card-content>
+      </ion-card>
+    </ion-content>
+  </ion-page>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import {
+  IonPage,
+  IonContent,
+  IonToolbar,
+  IonTitle,
+  IonCard,
+  IonCardHeader,
+  IonCardTitle,
+  IonCardContent,
+  IonList,
+  IonItem,
+  IonLabel
+} from "@ionic/vue";
+
+export default defineComponent({
+  name: "WalletOverview",
+  components: {
+    IonPage,
+    IonContent,
+    IonToolbar,
+    IonTitle,
+    IonCard,
+    IonCardHeader,
+    IonCardTitle,
+    IonCardContent,
+    IonList,
+    IonItem,
+    IonLabel
+  }
+});
+</script>
+
+<style scoped>
+ion-card-header {
+    padding-top: 0.4em;
+    padding-bottom: 0.4em;
+}
+
+ion-card-title {
+    font-size: 1rem;
+    font-weight: bold;
+}
+
+.icon-right {
+    float: right;
+}
+</style>
