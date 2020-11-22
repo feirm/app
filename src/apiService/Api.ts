74f91f3149274899b719bf6e2219d0ee4a637e26
Jack Coble <jack@coble.uk>
2020-11-22 17:24:30 +0000

    Update API location

--- src/apiService/Api.ts
+++ src/apiService/Api.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // Axios instance for Tatsuya authentication API
 const tatsuyaApi = axios.create({
-    baseURL: 'https://tatsuya.feirm.com/api/'
+    baseURL: 'http://127.0.0.1:3000/api/'
 })
 
 // Export all Axios instances
