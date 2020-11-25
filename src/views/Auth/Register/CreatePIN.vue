0ea7c22df0feafbab1d079dbc5fb2f54469b571f
Jack Coble <jack@coble.uk>
2020-11-25 18:53:02 +0000

    Beginning of structure for PIN registration page

--- src/views/Auth/Register/CreatePIN.vue
+++ src/views/Auth/Register/CreatePIN.vue
@@ -1,21 +1,64 @@
 <template>
-    <ion-page>
-        <ion-content :fullscreen="true" class="ion-padding"></ion-content>
-    </ion-page>
+  <ion-page>
+    <ion-header class="ion-no-border">
+      <ion-toolbar class="ion-text-center">
+        <ion-buttons slot="start">
+          <ion-back-button></ion-back-button>
+        </ion-buttons>
+        <ion-buttons slot="secondary">
+          <ion-button>
+            <ion-icon
+              slot="icon-only"
+              :icon="informationCircleOutline"
+            ></ion-icon>
+          </ion-button>
+        </ion-buttons>
+      </ion-toolbar>
+    </ion-header>
+    <ion-content :fullscreen="true" class="ion-padding">
+      <ion-item>
+        <ion-input type="number" v-model="pin"></ion-input>
+        <ion-button>Submit</ion-button>
+      </ion-item>
+    </ion-content>
+  </ion-page>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { defineComponent, ref, reactive } from "vue";
 import {
-    IonPage,
-    IonContent
-} from '@ionic/vue';
+  IonPage,
+  IonContent,
+  IonHeader,
+  IonToolbar,
+  IonButtons,
+  IonBackButton,
+  IonIcon,
+  IonItem,
+  IonInput,
+  IonButton
+} from "@ionic/vue";
+import { informationCircleOutline } from "ionicons/icons";
+import { useStore } from "vuex";
 
 export default defineComponent({
-    name: 'CreatePIN',
-    components: {
-        IonPage,
-        IonContent
-    }
-})
+  name: "CreatePIN",
+  components: {
+    IonPage,
+    IonContent,
+    IonHeader,
+    IonToolbar,
+    IonButtons,
+    IonBackButton,
+    IonIcon,
+    IonItem,
+    IonInput,
+    IonButton
+  },
+  setup() {
+    return {
+      informationCircleOutline,
+    };
+  },
+});
 </script>
