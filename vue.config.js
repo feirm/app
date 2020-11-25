84a28c6e2441ed4457de43774adc122b12881a2e
Jack Coble <jack@coble.uk>
2020-11-25 11:24:09 +0000

    Update Vue config to include support for WASM

--- vue.config.js
+++ vue.config.js
@@ -5,5 +5,17 @@ module.exports = {
             skipWaiting: true,
             exclude: [/_redirects/],
         }
+    },
+    configureWebpack: {
+        module: {
+            noParse: /\.wasm$/,
+            rules: [
+                {
+                    test: /\.wasm$/,
+                    loaders: ['base64-loader'],
+                    type: 'javascript/auto'
+                }
+            ]
+        }
     }
 }
