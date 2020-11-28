b1f65568c71376edcca0def28011233c7159de01
Jack Coble <jack@coble.uk>
2020-11-28 15:54:54 +0000

    Create Vuex actions, mutations and getters to manage the authentication tokens

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,4 +1,6 @@
+/* eslint-disable @typescript-eslint/camelcase */
 import { createStore } from 'vuex';
+import jwt_decode from "jwt-decode";
 
 export const store = createStore({
     state: {
@@ -6,6 +8,10 @@ export const store = createStore({
             username: "",
             email: "",
             password: "",
+        },
+        authentication: {
+            accessToken: "",
+            refreshToken: ""
         }
     },
     mutations: {
@@ -24,20 +30,56 @@ export const store = createStore({
             state.registration.username = "";
             state.registration.email = "";
             state.registration.password = "";
+        },
+        setAuthenticationTokens(state, authenticationToken) {
+            state.authentication.accessToken = authenticationToken.accessToken;
+            state.authentication.refreshToken = authenticationToken.refreshToken;
+
+            localStorage.setItem("accessToken", authenticationToken.accessToken);
+            localStorage.setItem("refreshToken", authenticationToken.refreshToken);
         }
     },
     actions: {
         initialize({ commit }) {
-            commit("registerUsername", "");
-            commit("registerEmail", "");
-            commit("registerPassword", "");
+            // Set any authentication tokens
+            const accessToken = localStorage.getItem("accessToken");
+            const refreshToken = localStorage.getItem("refreshToken");
+
+            if (accessToken && refreshToken) {
+                const authenticationTokens = {
+                    accessToken: accessToken,
+                    refreshToken: refreshToken
+                }
+
+                commit("setAuthenticationTokens", authenticationTokens);
+            }
         },
         clearRegistrationState({ commit }) {
             commit("clearRegistration")
+        },
+        login({ commit }, authenticationToken) {
+            commit("setAuthenticationTokens", authenticationToken);
         }
     },
     getters: {
-        isUserLoggedIn: () => false, // Set to false for testing purposes.
+        isUserLoggedIn: (state, getters) => {
+            return !!state.authentication.accessToken && getters.getAccessTokenExpirationDate > new Date()
+        },
+        getAccessTokenExpirationDate: state => {
+            if (!state.authentication.accessToken) {
+                return null;
+            }
+
+            const token: any = jwt_decode(state.authentication.accessToken);
+            if (!token.exp) {
+                return null;
+            }
+
+            const date = new Date(0)
+            date.setUTCSeconds(token.exp);
+            return date;
+        },
+        getAccessToken: state => state.authentication.accessToken,
         getRegistration: state => state.registration // Return the entire registration state
     }
 })
