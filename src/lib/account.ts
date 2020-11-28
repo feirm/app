6002fbc63e38afba52955446eed7400c99d9ab85
Jack Coble <jack@coble.uk>
2020-11-28 18:47:08 +0000

    Start handling the account decryption process

--- src/lib/account.ts
+++ src/lib/account.ts
@@ -41,7 +41,10 @@ async function generateAccount(
   const rootKey: Uint8Array = window.crypto.getRandomValues(new Uint8Array(32));
 
   const identityKeyString = bufferToHex(rootKey) + "identity";
-  const identityKey = await window.crypto.subtle.digest("SHA-256", new TextEncoder().encode(identityKeyString));
+  const identityKey = await window.crypto.subtle.digest(
+    "SHA-256",
+    new TextEncoder().encode(identityKeyString)
+  );
 
   // Generate a signing ed25519 keypair from a seed (identityKey)
   const rootKeyPair = nacl.sign.keyPair.fromSeed(new Uint8Array(identityKey));
@@ -49,21 +52,22 @@ async function generateAccount(
   // Fetch a temporary registration token and sign it
   let tokenId = "";
   let signature: any = "";
-  await tatsuyaApi.getRegistrationToken().then(res => {
+  await tatsuyaApi.getRegistrationToken().then((res) => {
     // Set ID
     tokenId = res.data.id;
 
     // Create signature
     const encoded = new TextEncoder().encode(res.data.nonce);
     signature = nacl.sign.detached(encoded, rootKeyPair.secretKey);
-  })
+  });
 
   // Create Salt for RootKey encryption
   const rootKeySalt = window.crypto.getRandomValues(new Uint8Array(16));
 
   // Encrypt rootKey with AES-256-CBC using the secretKey and rootKeySalt
+  const rootKeyBytes = aes.utils.utf8.toBytes(bufferToHex(rootKey));
   const aesCBC = new aes.ModeOfOperation.cbc(secretKey.hash, rootKeySalt);
-  const cipherText = aesCBC.encrypt(rootKey);
+  const cipherText = aesCBC.encrypt(rootKeyBytes);
 
   const account = {
     username: username,
@@ -71,16 +75,50 @@ async function generateAccount(
     rootPasswordSalt: bufferToHex(saltArray),
     rootPublicKey: bufferToHex(rootKeyPair.publicKey),
     encryptedRootKey: {
-      cipherText: aes.utils.hex.fromBytes(cipherText),
+      cipherText: bufferToHex(cipherText),
       iv: bufferToHex(rootKeySalt),
     },
     token: {
       id: tokenId,
-      signature: bufferToHex(signature)
-    }
+      signature: bufferToHex(signature),
+    },
   } as Account;
 
   return account;
 }
 
-export { Account, generateAccount };
+// Handle an account login request (fetch, decrypt, sign and submit)
+async function loginAccount(
+  username: string,
+  password: string
+): Promise<Account> {
+  // Fetch the encrypted account blob
+  const account = {} as Account;
+
+  await tatsuyaApi
+    .fetchEncryptedAccount(username)
+    .then(async (res) => {
+      // Derive the secret key using Argon2 along with password + salt
+      const secretKey = await argon2.hash({
+        pass: password,
+        salt: res.data.rootPasswordSalt,
+        type: argon2.ArgonType.Argon2id,
+        hashLen: 32,
+      });
+
+      // Attempt to decrypt the account with AES
+      const rootKeySalt = aes.utils.hex.toBytes(res.data.encryptedRootKey.iv).slice(0, 16);
+      const aesCBC = new aes.ModeOfOperation.cbc(secretKey.hash, rootKeySalt);
+      
+      try {
+        const account = aesCBC.decrypt(aes.utils.hex.toBytes(res.data.encryptedRootKey.cipherText));
+        console.log("ROOT KEY:", bufferToHex(account))
+      } catch (e) {
+        throw new Error(e)
+      }
+    });
+
+  return account;
+}
+
+export { Account, generateAccount, loginAccount };
