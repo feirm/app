f6b1f9d2b6b6c9093760f4c7f75ba8a42493bfa7
Jack Coble <jack@coble.uk>
2020-12-04 15:49:53 +0000

    Create contacts button

--- src/views/Contacts/Contacts.vue
+++ src/views/Contacts/Contacts.vue
@@ -1,21 +1,51 @@
 <template>
-    <ion-page>
-        <ion-content :fullscreen="true" class="ion-padding"></ion-content>
-    </ion-page>
+  <ion-page>
+    <ion-header>
+      <ion-toolbar class="ion-text-center">
+        <ion-buttons slot="start">
+        </ion-buttons>
+        <ion-buttons slot="secondary">
+          <ion-button>
+            <ion-icon slot="icon-only" :icon="addCircleOutline"></ion-icon>
+          </ion-button>
+        </ion-buttons>
+        <ion-title>Contacts</ion-title>
+      </ion-toolbar>
+    </ion-header>
+    <ion-content :fullscreen="true" class="ion-padding"></ion-content>
+  </ion-page>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
 import {
-    IonPage,
-    IonContent
+  IonPage,
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+  IonButtons,
+  IonButton,
+  IonIcon,
 } from "@ionic/vue";
+import { addCircleOutline } from "ionicons/icons";
 
 export default defineComponent({
-    name: "Contacts",
-    components: {
-        IonPage,
-        IonContent
-    }
-})
+  name: "Contacts",
+  components: {
+    IonPage,
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonContent,
+    IonButtons,
+    IonButton,
+    IonIcon,
+  },
+  setup() {
+    return {
+      addCircleOutline,
+    };
+  },
+});
 </script>
