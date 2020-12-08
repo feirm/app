23c7fc978b68a36e1c60e2a334d63285b17b1a58
Jack Coble <jack@coble.uk>
2020-12-08 21:23:55 +0000

    Begin deriving BIP44 wallets from mnemonic

--- src/lib/wallet.ts
+++ src/lib/wallet.ts
@@ -1,4 +1,5 @@
-import { entropyToMnemonic } from "bip39";
+import { entropyToMnemonic, mnemonicToSeed } from "bip39";
+import { fromSeed } from "bip32";
 import bufferToHex from './bufferToHex';
 
 // Wallet interface
@@ -13,6 +14,32 @@ async function GenerateMnemonic(): Promise<string> {
     return mnemonic;
 }
 
+// Take a mnemonic and derive an BTC BIP-44 wallet
+async function DeriveWallet(mnemonic: string): Promise<any> {
+    // Derive seed from mnemonic
+    const seed = await mnemonicToSeed(mnemonic);
+
+    // Feirm Network
+    const feirmNetwork = {
+        messagePrefix: "\x18DarkNet Signed Message:\n",
+        bip32: {
+            public: 0x0488b21e,
+            private: 0x0488ade4
+        },
+        pubKeyHash: 0xc,
+        scriptHash: 0x39,
+        wif: 0x37
+    }
+
+    // BIP-44
+    const rootKey = fromSeed(seed, feirmNetwork);
+    const addressNode = rootKey.derivePath("m/44'/193'/0'/0");
+
+    console.log("xprivkey:", addressNode.toBase58())
+    console.log("xpubkey:", addressNode.neutered().toBase58())
+}
+
 export {
-    GenerateMnemonic
+    GenerateMnemonic,
+    DeriveWallet
 }
