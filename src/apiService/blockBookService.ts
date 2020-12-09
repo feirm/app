fbd98a2cc95c21903ff8b343eb1a7b09296e935c
Jack Coble <jack@coble.uk>
2020-12-09 22:27:09 +0000

    Update Blockbook API version

--- src/apiService/blockBookService.ts
+++ src/apiService/blockBookService.ts
@@ -3,6 +3,6 @@ import { blockBookApi } from "@/apiService/Api";
 export default {
     // Return data for xpub
     getXpub(xpub: string) {
-        return blockBookApi.get(`/xpub/${xpub}`);
+        return blockBookApi.get(`/v2/xpub/${xpub}`);
     }
 }
