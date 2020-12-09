6923606f8ceeb3969306cdc3c854dc8744cd6b00
Jack Coble <jack@coble.uk>
2020-12-09 12:37:22 +0000

    Improve presentation of the wallet overview page

--- src/views/Wallet/Overview.vue
+++ src/views/Wallet/Overview.vue
@@ -6,16 +6,20 @@
     </ion-toolbar>
     </ion-header>
     <ion-content :fullscreen="true" class="ion-padding">
-      <ion-text class="ion-text-center">
-        <p v-if="!wallet">
-          It appears that you do not have a wallet. Are you ready to start?
-        </p>
-        <ion-button v-if="!wallet" expand="block" @click="router.push({ path: '/tabs/wallet/newSeed' })">Create a wallet</ion-button>
-      </ion-text>
-      <ion-card button="true">
-        <ion-card-title>{{ wallet }}</ion-card-title>
-      </ion-card>
+      <ion-grid>
+        <ion-row>
+          <ion-col class="ion-text-center">
+            <ion-img src="/assets/logo.png"></ion-img>
+            <h1>Feirm Wallet</h1>
+            <p>It appears that you don't have a wallet created. Would you like to get started, or restore from a backup? 💰</p>
+          </ion-col>
+        </ion-row>
+      </ion-grid>
     </ion-content>
+    <ion-footer v-if="!wallet" class="ion-no-border ion-padding ion-text-center">
+        <ion-button expand="block" @click="router.push({ path: '/tabs/wallet/newSeed' })">Get Started</ion-button>
+        <ion-note color="dark">Restore from backup</ion-note>
+    </ion-footer>
   </ion-page>
 </template>
 
@@ -28,9 +32,11 @@ import {
   IonTitle,
   IonHeader,
   IonButton,
-  IonText,
-  IonCard,
-  IonCardTitle
+  IonFooter,
+  IonGrid,
+  IonRow,
+  IonCol,
+  IonNote
 } from "@ionic/vue";
 import { walletOutline } from "ionicons/icons";
 import { GenerateMnemonic, Wallet } from "@/lib/wallet";
@@ -45,9 +51,11 @@ export default defineComponent({
     IonTitle,
     IonHeader,
     IonButton,
-    IonText,
-    IonCard,
-    IonCardTitle
+    IonFooter,
+    IonGrid,
+    IonRow,
+    IonCol,
+    IonNote
   },
   data() {
     return {
@@ -73,3 +81,21 @@ export default defineComponent({
   },
 });
 </script>
+
+<style scoped>
+ion-img {
+  margin: 0 auto;
+  width: 100px;
+}
+
+ion-grid {
+  height: 100%;
+}
+
+ion-row {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100%;
+}
+</style>
