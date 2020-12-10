691e4f3cdb26b2d08ac5b53d842efd42947d695d
Jack Coble <jack@coble.uk>
2020-12-10 21:04:01 +0000

    Start of settings page

---
+++ src/views/Wallet/Settings/Settings.vue
@@ -0,0 +1,39 @@
+<template>
+  <ion-page>
+    <ion-header>
+      <ion-toolbar class="ion-text-center">
+        <ion-buttons slot="start">
+          <ion-back-button></ion-back-button>
+        </ion-buttons>
+        <ion-title>Settings</ion-title>
+      </ion-toolbar>
+    </ion-header>
+    <ion-content :fullscreen="true" class="ion-padding"> </ion-content>
+  </ion-page>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import {
+  IonPage,
+  IonContent,
+  IonHeader,
+  IonToolbar,
+  IonButtons,
+  IonBackButton,
+  IonTitle,
+} from "@ionic/vue";
+
+export default defineComponent({
+  name: "Settings",
+  components: {
+    IonPage,
+    IonContent,
+    IonHeader,
+    IonToolbar,
+    IonButtons,
+    IonBackButton,
+    IonTitle,
+  },
+});
+</script>
