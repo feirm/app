8f0b9649cca8a8f2591e7c0c6aaa879f1173f4cb
Jack Coble <jack@coble.uk>
2020-12-10 15:57:42 +0000

    Derive a Feirm wallet after backing up mnemonic

--- src/views/Wallet/BackupSeed.vue
+++ src/views/Wallet/BackupSeed.vue
@@ -1,44 +1,77 @@
 <template>
-    <ion-page>
-        <ion-header>
-            <ion-toolbar class="ion-text-center">
-                <ion-buttons slot="start">
-                    <ion-back-button></ion-back-button>
-                </ion-buttons>
-                <ion-title>Verifying your seed</ion-title>
-            </ion-toolbar>
-        </ion-header>
-        <ion-content class="ion-padding" :fullscreen="true">
-            <ion-text>
-                <p>To make sure that you have written down your seed, we are going to ask you to enter 3 words of the seed. Please enter the {{ ordinal(firstWordNumber) }}, {{ ordinal(secondWordNumber) }} and {{ ordinal(thirdWordNumber) }} word of the seed phrase in order to continue.</p>
-            </ion-text>
+  <ion-page>
+    <ion-header>
+      <ion-toolbar class="ion-text-center">
+        <ion-buttons slot="start">
+          <ion-back-button></ion-back-button>
+        </ion-buttons>
+        <ion-title>Verifying your seed</ion-title>
+      </ion-toolbar>
+    </ion-header>
+    <ion-content class="ion-padding" :fullscreen="true">
+      <ion-text>
+        <p>
+          To make sure that you have written down your seed, we are going to ask
+          you to enter 3 words of the seed. Please enter the
+          {{ ordinal(firstWordNumber) }}, {{ ordinal(secondWordNumber) }} and
+          {{ ordinal(thirdWordNumber) }} word of the seed phrase in order to
+          continue.
+        </p>
+      </ion-text>
 
-            <!-- Seed phrase word entry -->
-            <ion-item-group>
-                <ion-item>
-                    <ion-label position="stacked">{{ ordinal(firstWordNumber) }} word</ion-label>
-                    <ion-input v-model="firstWord" type="text"></ion-input>
-                </ion-item>
-                <ion-item>
-                    <ion-label position="stacked">{{ ordinal(secondWordNumber) }} word</ion-label>
-                    <ion-input v-model="secondWord" type="text"></ion-input>
-                </ion-item>
-                <ion-item>
-                    <ion-label position="stacked">{{ ordinal(thirdWordNumber) }} word</ion-label>
-                    <ion-input v-model="thirdWord" type="text"></ion-input>
-                </ion-item>
-            </ion-item-group>
-
-        </ion-content>
-        <ion-footer class="ion-no-border ion-padding">
-            <ion-button expand="block" @click="verify">Verify</ion-button>
-        </ion-footer>
-    </ion-page>
+      <!-- Seed phrase word entry -->
+      <ion-item-group>
+        <ion-item>
+          <ion-label position="stacked"
+            >{{ ordinal(firstWordNumber) }} word</ion-label
+          >
+          <ion-input v-model="firstWord" type="text"></ion-input>
+        </ion-item>
+        <ion-item>
+          <ion-label position="stacked"
+            >{{ ordinal(secondWordNumber) }} word</ion-label
+          >
+          <ion-input v-model="secondWord" type="text"></ion-input>
+        </ion-item>
+        <ion-item>
+          <ion-label position="stacked"
+            >{{ ordinal(thirdWordNumber) }} word</ion-label
+          >
+          <ion-input v-model="thirdWord" type="text"></ion-input>
+        </ion-item>
+      </ion-item-group>
+    </ion-content>
+    <ion-footer class="ion-no-border ion-padding">
+      <ion-button expand="block" @click="verify">Verify</ion-button>
+    </ion-footer>
+  </ion-page>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
 import {
+  IonPage,
+  IonContent,
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonText,
+  IonBackButton,
+  IonButton,
+  IonFooter,
+  IonItemGroup,
+  IonItem,
+  IonInput,
+  IonLabel,
+  alertController,
+} from "@ionic/vue";
+import { useStore } from "vuex";
+import { useRouter } from "vue-router";
+import { DeriveWallet } from "@/lib/wallet";
+
+export default defineComponent({
+  name: "NewSeed",
+  components: {
     IonPage,
     IonContent,
     IonHeader,
@@ -52,92 +85,77 @@ import {
     IonItem,
     IonInput,
     IonLabel,
-    alertController
-} from "@ionic/vue";
-import { useStore } from "vuex";
-import { DeriveWallet } from "@/lib/wallet";
-import { useRouter } from "vue-router";
+  },
+  data() {
+    return {
+      firstWordNumber: Math.floor(Math.random() * 23) + 1,
+      secondWordNumber: Math.floor(Math.random() * 23) + 1,
+      thirdWordNumber: Math.floor(Math.random() * 23) + 1,
 
-export default defineComponent({
-    name: "NewSeed",
-    components: {
-        IonPage,
-        IonContent,
-        IonHeader,
-        IonToolbar,
-        IonTitle,
-        IonText,
-        IonBackButton,
-        IonButton,
-        IonFooter,
-        IonItemGroup,
-        IonItem,
-        IonInput,
-        IonLabel
+      firstWord: "",
+      secondWord: "",
+      thirdWord: "",
+    };
+  },
+  methods: {
+    ordinal: function (n: any) {
+      const s = ["th", "st", "nd", "rd"],
+        v = n % 100;
+      return n + (s[(v - 20) % 10] || s[v] || s[0]);
     },
-    data() {
-        return {
-            firstWordNumber: Math.floor(Math.random() * 23) + 1,
-            secondWordNumber: Math.floor(Math.random() * 23) + 1,
-            thirdWordNumber: Math.floor(Math.random() * 23) + 1,
+    async verify() {
+      const mnemonic = this.store.getters.getWalletMnemonic;
+      const splitMnemonic = mnemonic.split(" ");
 
-            firstWord: "",
-            secondWord: "",
-            thirdWord: ""
-        }
+      const alert = await alertController.create({
+        header: "Verification failed!",
+        message: "Please check that you inputted the seed words correctly.",
+        buttons: ["Okay"],
+      });
+
+      if (!splitMnemonic.includes(this.firstWord)) {
+        return alert.present();
+      }
+
+      if (!splitMnemonic.includes(this.secondWord)) {
+        return alert.present();
+      }
+
+      if (!splitMnemonic.includes(this.thirdWord)) {
+        return alert.present();
+      }
+
+      // If all is valid, then move on...
+      const congratulationsAlert = await alertController.create({
+        header: "Congratulations! 🥳",
+        message: "You have successfully verified your 24-word mnemonic!",
+        buttons: [
+          {
+            text: "Okay",
+            handler: async () => {
+              // Generate a wallet with Feirm as a coin
+              await DeriveWallet(
+                this.store.getters.getWalletMnemonic,
+                "xfe"
+              ).then(() => {
+                this.router.push({ path: "/tabs/wallet" });
+              });
+            },
+          },
+        ],
+      });
+
+      return congratulationsAlert.present();
     },
-    methods: {
-        ordinal: function(n: any) {
-            const s = ["th", "st", "nd", "rd"],
-            v = n % 100;
-            return n + (s[(v - 20) % 10] || s[v] || s[0]);
-        },
-        async verify() {
-            const mnemonic = this.store.getters.getWalletMnemonic;
-            const splitMnemonic = mnemonic.split(" ");
+  },
+  setup() {
+    const store = useStore();
+    const router = useRouter();
 
-            const alert = await alertController.create({
-                header: "Verification failed!",
-                message: "Please check that you inputted the seed words correctly.",
-                buttons: ["Okay"]
-            });
-
-            if (!splitMnemonic.includes(this.firstWord)) {
-                return alert.present();
-            }
-
-            if (!splitMnemonic.includes(this.secondWord)) {
-                return alert.present();
-            }
-
-            if (!splitMnemonic.includes(this.thirdWord)) {
-                return alert.present();
-            }
-
-            // If all is valid, then move on...
-            const congratulationsAlert = await alertController.create({
-                header: "Congratulations! 🥳",
-                message: "You have successfully verified your 24-word mnemonic!",
-                buttons: [{
-                    text: "Okay",
-                    handler: async () => {
-                        await DeriveWallet(this.store.getters.getWalletMnemonic);
-                        this.router.push({ path: "/tabs/wallet" })
-                    }
-                }]
-            })
-
-            return congratulationsAlert.present();
-        }
-    },
-    setup() {
-        const store = useStore();
-        const router = useRouter();
-
-        return {
-            store,
-            router
-        }
-    }
-})
+    return {
+      store,
+      router,
+    };
+  },
+});
 </script>
