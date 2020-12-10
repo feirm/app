60afe3ff0e65f8ef4c83a7b866c145d28bf6d3ad
Jack Coble <jack@coble.uk>
2020-12-10 15:22:49 +0000

    Move wallet into its own Vuex module

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,36 +1,13 @@
 /* eslint-disable @typescript-eslint/camelcase */
 import { createStore } from "vuex";
-import { Wallet } from "@/lib/wallet";
+
+// Vuex modules
 import { auth } from "./modules/auth";
+import { wallet } from "./modules/wallet";
 
 export const store = createStore({
   modules: {
     auth,
-  },
-  state: {
-    wallet: {} as Wallet,
-  },
-  mutations: {
-    // Mutations for wallet
-    setMnemonic(state, mnemonic) {
-      state.wallet.mnemonic = mnemonic;
-    },
-    setWalletState(state, wallet) {
-      state.wallet = wallet;
-    },
-  },
-  actions: {
-    initialize({ commit }) {
-      const wallet = localStorage.getItem("wallet");
-
-      if (wallet) {
-        commit("setWalletState", JSON.parse(wallet));
-      }
-    },
-  },
-  getters: {
-    // Wallet
-    getWalletMnemonic: (state) => state.wallet.mnemonic,
-    getWallet: (state) => state.wallet,
-  },
+    wallet
+  }
 });
---
+++ src/store/modules/wallet/index.ts
@@ -0,0 +1,55 @@
+export const wallet = {
+  state: {
+    wallet: {
+      id: "",
+      mnemonic: "",
+      // We can have multiple coins in a wallet
+      coins: [
+        {
+          name: "",
+          rootKey: "",
+          extendedPrivateKey: "",
+          extendedPublicKey: "",
+          index: 0,
+          blockbook: "",
+
+          // Network information
+          network: {
+            bip44: 0,
+            txVersion: 0,
+            messagePrefix: "",
+            bech32: "",
+            bip32: {
+              private: 0,
+              public: 0,
+            },
+            pubKeyHash: 0,
+            scriptHash: 0,
+            wif: 0,
+          },
+        },
+      ],
+    },
+  },
+  mutations: {
+    setWalletMnemonic(state, mnemonic) {
+      state.wallet.mnemonic = mnemonic;
+    },
+    setWalletState(state, wallet) {
+      state.wallet = wallet;
+    },
+  },
+  actions: {
+    initialize({ commit }) {
+      const wallet = localStorage.getItem("wallet");
+
+      if (wallet) {
+        commit("setWalletState", JSON.parse(wallet));
+      }
+    },
+  },
+  getters: {
+      getWalletMnemonic: state => state.wallet.mnemonic,
+      getWallet: state => state.wallet
+  }
+};
