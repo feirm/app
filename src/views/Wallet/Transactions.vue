fba0e7fcbb27507371ca54596b0f04996fd79e40
Jack Coble <jack@coble.uk>
2020-12-31 13:01:08 +0000

    Wrap transaction history actions in a loading alert

--- src/views/Wallet/Transactions.vue
+++ src/views/Wallet/Transactions.vue
@@ -59,6 +59,8 @@ import {
   IonItemGroup,
   IonText,
   IonIcon,
+  loadingController,
+  alertController,
 } from "@ionic/vue";
 import { Coin, FindWallet } from "@/lib/wallet";
 import axios from "axios";
@@ -91,72 +93,102 @@ export default defineComponent({
   },
   methods: {
     async ionViewWillEnter() {
-      // Fetch coin information from localStorage
-      const ticker = this.$route.params.coin as string;
+      // Wrap in a loading controller
+      await loadingController
+        .create({
+          message: "Fetching transactions...",
+        })
+        .then((a) => {
+          a.present()
+            .then(async () => {
+              // Fetch coin information from localStorage
+              const ticker = this.$route.params.coin as string;
 
-      await FindWallet(ticker).then((coin) => {
-        this.coin = coin;
-        this.ticker = coin.ticker;
-      });
+              await FindWallet(ticker).then((coin) => {
+                this.coin = coin;
+                this.ticker = coin.ticker;
+              });
 
-      // Fetch transaction history
-      const txHistory = await axios.get(
-        "https://cors-anywhere.feirm.com/" +
-          this.coin.blockbook +
-          "/api/v2/xpub/" +
-          this.coin.extendedPublicKey +
-          "?details=txs&tokens=used"
-      );
+              // Fetch transaction history
+              const txHistory = await axios.get(
+                "https://cors-anywhere.feirm.com/" +
+                  this.coin.blockbook +
+                  "/api/v2/xpub/" +
+                  this.coin.extendedPublicKey +
+                  "?details=txs&tokens=used"
+              );
 
-      // Representation of a TX
-      interface Transaction {
-        txid: string;
-        blockTime: string;
-        value: string;
-        isOutgoing: boolean;
-      }
+              // Representation of a TX
+              interface Transaction {
+                txid: string;
+                blockTime: string;
+                value: string;
+                isOutgoing: boolean;
+              }
 
-      // Iterate over each transaction and format them
-      for (let i = 0; i < txHistory.data.transactions.length; i++) {
-        const tx = txHistory.data.transactions[i];
+              // Iterate over each transaction and format them
+              for (let i = 0; i < txHistory.data.transactions.length; i++) {
+                const tx = txHistory.data.transactions[i];
 
-        // New transaction instance
-        const newTx = {} as Transaction;
+                // New transaction instance
+                const newTx = {} as Transaction;
 
-        // Set id
-        newTx.txid = tx.txid;
+                // Set id
+                newTx.txid = tx.txid;
 
-        // Formatted time
-        newTx.blockTime = DateTime.fromSeconds(parseInt(tx.blockTime)).toFormat(
-          "dd/MM/yyyy, hh:mm a"
-        );
+                // Formatted time
+                newTx.blockTime = DateTime.fromSeconds(
+                  parseInt(tx.blockTime)
+                ).toFormat("dd/MM/yyyy, hh:mm a");
 
-        // Determine if a transaction is incoming or outgoing
-        // by checking if a change address the user owns is present.
-        // If a change address the user owns is present, the the TX is outgoing
+                // Determine if a transaction is incoming or outgoing
+                // by checking if a change address the user owns is present.
+                // If a change address the user owns is present, the the TX is outgoing
 
-        // Iterate over the known tokens used for these transactions
-        for (let i = 0; i < txHistory.data.tokens.length; i++) {
-          const token = txHistory.data.tokens[i];
-          
-          // Iterate over each output and determine whether or not a change address we own is present
-          for (let j = 0; j < tx.vout.length; j++) {
-            // Find matching addresses and check if they are part of change we own
-            if (tx.vout[j].addresses[0] === token.name && token.path.includes("/0'/1/") === true) {
-              // Transaction must be outgoing
-              newTx.isOutgoing = true;
-            }
+                // Iterate over the known tokens used for these transactions
+                for (let i = 0; i < txHistory.data.tokens.length; i++) {
+                  const token = txHistory.data.tokens[i];
 
-            // TODO Find the value of the transaction for the address we don't own
-            if (tx.vout[j].addresses[0] !== token.name) {
-              newTx.value = new BigNumber(tx.vout[j].value).dividedBy(100000000).toString()
-            }
-          }
-        }
+                  // Iterate over each output and determine whether or not a change address we own is present
+                  for (let j = 0; j < tx.vout.length; j++) {
+                    // Find matching addresses and check if they are part of change we own
+                    if (
+                      tx.vout[j].addresses[0] === token.name &&
+                      token.path.includes("/0'/1/") === true
+                    ) {
+                      // Transaction must be outgoing
+                      newTx.isOutgoing = true;
+                    }
 
-        // Append to array of transactions
-        this.txs.push(newTx);
-      }
+                    // TODO Find the value of the transaction for the address we don't own
+                    if (tx.vout[j].addresses[0] !== token.name) {
+                      newTx.value = new BigNumber(tx.vout[j].value)
+                        .dividedBy(100000000)
+                        .toString();
+                    }
+                  }
+                }
+
+                // Append to array of transactions
+                this.txs.push(newTx);
+              }
+
+              // Dismiss the loading alert
+              a.dismiss();
+            })
+            .catch(async (e) => {
+              a.dismiss();
+
+              // Show error alert
+              const alert = await alertController.create({
+                header: "Error fetching transactions!",
+                message: e,
+                buttons: ["Close"],
+              });
+
+              return alert.present();
+            });
+        });
     },
     openTx(hash: string) {
       window.open(this.coin.blockbook + "/tx/" + hash);
