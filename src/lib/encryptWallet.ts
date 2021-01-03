347b848069696600802b28ca64072a47ce41e23b
Jack Coble <jack@coble.uk>
2021-01-03 17:18:35 +0000

    Function to decrypt wallet

--- src/lib/encryptWallet.ts
+++ src/lib/encryptWallet.ts
@@ -2,6 +2,61 @@ import { store } from "@/store";
 import { ModeOfOperation, padding, utils } from "aes-js";
 import { ArgonType, hash } from "argon2-browser"
 import bufferToHex from "./bufferToHex";
+import hexStringToBytes from "./hexStringToBytes";
+
+async function decryptWallet(pin: string) {
+    // Fetch the wallet from Vuex state
+    const wallet = store.getters.getWallet;
+
+    // Derive the encryption key from our PIN
+    const secretKey = await hash({
+        pass: pin,
+        type: ArgonType.Argon2id,
+        hashLen: 32,
+        salt: hexStringToBytes(wallet.encryption.encryptionKeySalt)
+    })
+
+    // Convert the encryption data IV to a valid byte source
+    const iv = utils.hex.toBytes(wallet.encryption.encryptionIv);
+
+    // Create a new AES-CBC decipher
+    const aesCbc = new ModeOfOperation.cbc(secretKey.hash, iv);
+
+    // Attempt to decrypt the mnemonic
+    try {
+        const mnemonic = aesCbc.decrypt(utils.hex.toBytes(wallet.mnemonic));
+        wallet.mnemonic = utils.utf8.fromBytes(mnemonic);
+    } catch (e) {
+        throw new Error("Unable to decrypt mnemonic. Please make sure your PIN is correct!")
+    }
+
+    // Attempt to decrypt all of the coins
+    for (let i = 0; i < wallet.coins.length; i++) {
+        const coin = wallet.coins[i];
+
+        // Check that the coin has the encryption property
+        if (coin.isEncrypted) {
+            // Attempt to decrypt the root key
+            try {
+                const rootKey = aesCbc.decrypt(utils.hex.toBytes(coin.rootKey))
+                wallet.rootKey = utils.utf8.fromBytes(padding.pkcs7.strip(rootKey));
+            } catch (e) {
+                throw new Error("Unable to decrypt root key!")
+            }
+
+            // Attempt to decrypt extended private key
+            try {
+                const exPrivKey = aesCbc.decrypt(utils.hex.toBytes(coin.extendedPrivateKey))
+                wallet.extendedPrivateKey = utils.utf8.fromBytes(padding.pkcs7.strip(exPrivKey));
+            } catch (e) {
+                throw new Error("Unable to decrypt extended private key!")
+            }
+        }
+    }
+
+    // Set the wallet unlocked state
+    store.commit("setWalletUnlockedState", wallet);
+}
 
 async function encryptWallet(pin: string) {
     // Convert the PIN into a more secure Argon2 key
@@ -56,8 +111,14 @@ async function encryptWallet(pin: string) {
 
     // Commit to store
     store.commit("setWalletState", wallet);
+
+    // As we have the PIN, we can decrypt the wallet and update our state for this session.
+    await decryptWallet(pin);
 }
 
+// TODO Encrypt individual coin
+
 export {
-    encryptWallet
+    encryptWallet,
+    decryptWallet
 }
