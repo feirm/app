c8170a1d3ba6954c53ab51f1445904eb01637ed6
Jack Coble <jack@coble.uk>
2021-01-05 21:38:24 +0000

    Re-design recent listing tiles

--- src/views/Marketplace/Overview.vue
+++ src/views/Marketplace/Overview.vue
@@ -2,20 +2,6 @@
   <ion-page>
     <ion-header>
       <ion-toolbar class="ion-text-center">
-        <ion-buttons slot="start">
-          <ion-button>
-            <ion-icon slot="icon-only"></ion-icon>
-          </ion-button>
-        </ion-buttons>
-        <ion-buttons slot="secondary">
-          <ion-button color="primary" @click="navToChat">
-            <ion-icon
-              slot="icon-only"
-              :icon="chatbubbleEllipsesOutline"
-            ></ion-icon>
-          </ion-button>
-        </ion-buttons>
-
         <ion-title>Marketplace</ion-title>
       </ion-toolbar>
     </ion-header>
@@ -44,38 +30,29 @@
         <ion-row>
           <ion-col size="3">
             <ion-tab-button>
-              <ion-icon
-                color="primary"
-                :icon="addCircleOutline"
-              ></ion-icon>
+              <ion-icon color="primary" :icon="addCircleOutline"></ion-icon>
               <ion-label>New</ion-label>
             </ion-tab-button>
           </ion-col>
           <ion-col size="3">
-            <ion-tab-button>
+            <ion-tab-button @click="navToChat">
               <ion-icon
                 color="primary"
-                :icon="fileTrayFullOutline"
+                :icon="chatbubbleEllipsesOutline"
               ></ion-icon>
+              <ion-label>Chat</ion-label>
+            </ion-tab-button>
+          </ion-col>
+          <ion-col size="3">
+            <ion-tab-button>
+              <ion-icon color="primary" :icon="fileTrayFullOutline"></ion-icon>
               <ion-label>History</ion-label>
             </ion-tab-button>
           </ion-col>
           <ion-col size="3">
             <ion-tab-button>
-              <ion-icon
-                color="primary"
-                :icon="storefrontOutline"
-              ></ion-icon>
+              <ion-icon color="primary" :icon="storefrontOutline"></ion-icon>
               <ion-label>Store</ion-label>
-            </ion-tab-button>
-          </ion-col>
-          <ion-col size="3">
-            <ion-tab-button>
-              <ion-icon
-                color="primary"
-                :icon="starOutline"
-              ></ion-icon>
-              <ion-label>Stars</ion-label>
             </ion-tab-button>
           </ion-col>
         </ion-row>
@@ -89,21 +66,28 @@
         <ion-row>
           <ion-col>
             <ion-card>
-              <ion-card-content> Listing 1 </ion-card-content>
-            </ion-card>
-          </ion-col>
-          <ion-col>
-            <ion-card>
-              <ion-card-content> Listing 2 </ion-card-content>
-            </ion-card>
-          </ion-col>
-          <ion-col>
-            <ion-card>
-              <ion-card-content> Listing 3 </ion-card-content>
+              <img
+                src="https://scontent-lht6-1.xx.fbcdn.net/v/t1.0-0/p180x540/56764508_2455886691110958_2158473021906485248_o.jpg?_nc_cat=108&ccb=2&_nc_sid=3b2858&_nc_ohc=xkW6Qq8YAL8AX-bu93a&_nc_ht=scontent-lht6-1.xx&tp=6&oh=94b8fad152ef09bf88c571798bbcd44f&oe=60188D98"
+              />
+              <ion-card-header>
+                <ion-text class="ion-text-left" color="dark">
+                  <b>£210</b>
+                </ion-text>
+              </ion-card-header>
+              <ion-card-content>
+                  <p style="color: white">Storage Containers</p>
+                  <p style="font-size: 12px">Sold by: sscontainersUk</p>
+              </ion-card-content>
             </ion-card>
           </ion-col>
         </ion-row>
       </ion-grid>
+
+      <!-- Necessary spacing to read last listings in full -->
+      <br />
+      <br />
+      <br />
+      <br />
     </ion-content>
   </ion-page>
 </template>
@@ -117,25 +101,23 @@ import {
   IonToolbar,
   IonTitle,
   IonCard,
+  IonCardHeader,
   IonCardContent,
   IonText,
   IonSearchbar,
   IonGrid,
   IonRow,
   IonCol,
-  IonButtons,
-  IonButton,
   IonTabButton,
   IonIcon,
   IonImg,
-  IonLabel
+  IonLabel,
 } from "@ionic/vue";
 import {
   chatbubbleEllipsesOutline,
   addCircleOutline,
   fileTrayFullOutline,
   storefrontOutline,
-  starOutline
 } from "ionicons/icons";
 import { useRouter } from "vue-router";
 
@@ -148,18 +130,17 @@ export default defineComponent({
     IonToolbar,
     IonTitle,
     IonCard,
+    IonCardHeader,
     IonCardContent,
     IonText,
     IonSearchbar,
     IonGrid,
     IonRow,
     IonCol,
-    IonButtons,
-    IonButton,
     IonTabButton,
     IonIcon,
     IonImg,
-    IonLabel
+    IonLabel,
   },
   methods: {
     navToChat() {
@@ -177,7 +158,6 @@ export default defineComponent({
       addCircleOutline,
       fileTrayFullOutline,
       storefrontOutline,
-      starOutline
     };
   },
 });
