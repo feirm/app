28d0830acf65a9ec9410bb0e1615671d4857a9be
Jack Coble <jack@coble.uk>
2021-01-10 23:24:56 +0000

    Move wallet unlock into main Vue instance on mount

--- src/App.vue
+++ src/App.vue
@@ -5,9 +5,12 @@
 </template>
 
 <script lang="ts">
-import { IonApp, IonRouterOutlet, loadingController } from '@ionic/vue';
+import { IonApp, IonRouterOutlet, loadingController, modalController } from '@ionic/vue';
 import { defineComponent, onMounted } from 'vue';
 import { useStore } from 'vuex';
+import PINVue from './components/Auth/PIN.vue';
+import { decryptWallet } from './lib/encryptWallet';
+import { Wallet } from './lib/wallet';
 
 export default defineComponent({
   name: 'App',
@@ -35,6 +38,68 @@ export default defineComponent({
     onMounted(async () => {
       // Pick a funny loading message at random
       const loadingMessage = funnyMessages[Math.floor(Math.random() * funnyMessages.length)]
+
+      // Check the Vuex state for a wallet and any encryption properties
+      const wallet = store.getters.getWallet;
+      const isWalletFormatEncrypted = store.getters.isWalletEncrypted;
+
+      // Maximum unlock attempts we can have is 3
+      const maxUnlockAttempts = 3;
+
+      // If the wallet isn't unlocked, try and unlock it
+      if (!store.getters.isWalletUnlocked) {
+        // Check that a mnemonic is present and that the wallet if encrypted
+        if (wallet.mnemonic && isWalletFormatEncrypted) {
+          // Enter a loop asking for PIN entry
+          for (let i = 0; i < maxUnlockAttempts; i++) {
+            // Create a modal for PIN entry
+            const pinEntry = await modalController.create({
+              component: PINVue,
+              componentProps: {
+                header: "Unlock your Wallet",
+                description: `You have ${
+                  maxUnlockAttempts - i
+                } unlock attempts remaining.`,
+              }
+            });
+
+            // Show the prompt
+            pinEntry.present();
+
+            // Fetch the PIN modal response
+            const pinResponse = await pinEntry.onDidDismiss();
+            const pin = pinResponse.data;
+
+            // Now that we've got a PIN, we can attempt to decrypt the wallet
+            try {
+              // Fetch the encrypted wallet from localStorage
+              const wallet = JSON.parse(localStorage.getItem("wallet")!) as Wallet;
+
+              // Decrypt the wallet with our PIN
+              const decryptedWallet = await decryptWallet(pin, wallet);
+
+              // Save decrypted wallet in Vuex
+              store.commit("setWalletState", decryptedWallet);
+
+              // Update the wallet unlock status
+              store.commit("setWalletUnlock", true);
+
+              // Set wallet PIN state
+              store.commit("setWalletPin", pin);
+
+              // Break the loop
+              break;
+            } catch (e) {
+              // If we've reached our maximum attempts, clear everything
+              if (maxUnlockAttempts - i === 1) {
+                store.commit("clearSessionState");
+                store.commit("deleteWalletState");
+                break;
+              }
+            }
+          }
+        }
+      }
 
       // Show a loading controller
       const load = await loadingController.create({
