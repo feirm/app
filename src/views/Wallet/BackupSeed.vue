ca4718f373b2fea53792efa9dca168111f6cf231
Jack Coble <jack@coble.uk>
2021-01-12 20:51:02 +0000

    Implement tapping backup method

--- src/views/Wallet/BackupSeed.vue
+++ src/views/Wallet/BackupSeed.vue
@@ -1,48 +1,37 @@
 <template>
   <ion-page>
-    <ion-header>
-      <ion-toolbar class="ion-text-center">
+    <ion-header class="ion-no-border">
+      <ion-toolbar class="ion-text-center" color="transparent">
         <ion-buttons slot="start">
           <ion-back-button></ion-back-button>
         </ion-buttons>
-        <ion-title>Verifying your seed</ion-title>
       </ion-toolbar>
     </ion-header>
-    <ion-content class="ion-padding" :fullscreen="true">
+    <ion-content class="ion-padding ion-text-left" :fullscreen="true">
+      <h3>Verifying your mnemonic backup</h3>
       <ion-text>
         <p>
-          To make sure that you have written down your seed, we are going to ask
-          you to enter 3 words of the seed. Please enter the
-          {{ ordinal(firstWordNumber) }}, {{ ordinal(secondWordNumber) }} and
-          {{ ordinal(thirdWordNumber) }} word of the seed phrase in order to
-          continue.
+          To make sure that you have written down your 24-word mnemonic properly, we are going to ask
+          you to reassemble the entire mnemonic from scratch.
         </p>
+        <p>If you tap on the wrong word, simply tap on it again to remove it from the assembled mnemonic.</p>
       </ion-text>
 
-      <!-- Seed phrase word entry -->
-      <ion-item-group>
-        <ion-item>
-          <ion-label position="stacked"
-            >{{ ordinal(firstWordNumber) }} word</ion-label
-          >
-          <ion-input v-model="firstWord" type="text"></ion-input>
-        </ion-item>
-        <ion-item>
-          <ion-label position="stacked"
-            >{{ ordinal(secondWordNumber) }} word</ion-label
-          >
-          <ion-input v-model="secondWord" type="text"></ion-input>
-        </ion-item>
-        <ion-item>
-          <ion-label position="stacked"
-            >{{ ordinal(thirdWordNumber) }} word</ion-label
-          >
-          <ion-input v-model="thirdWord" type="text"></ion-input>
-        </ion-item>
-      </ion-item-group>
+      <!-- Area where mnemonic is being assembled -->
+      <div class="mnemonic-area">
+        <ion-chip v-for="word in assembledMnemonic" v-bind:key="word" @click="removeWord(word)">
+          {{ word }}
+        </ion-chip>
+      </div>
+
+      <!-- Random pool of mnemonic words -->
+      <p v-show="splitMnemonic.length > 0">Available words: {{ splitMnemonic.length }}</p>
+      <ion-chip v-for="word in splitMnemonic" v-bind:key="word" @click="addWord(word)">
+        {{ word }}
+      </ion-chip>
     </ion-content>
     <ion-footer class="ion-no-border ion-padding">
-      <ion-button expand="block" @click="verify">Verify</ion-button>
+      <ion-button expand="block" @click="verify">Validate</ion-button>
     </ion-footer>
   </ion-page>
 </template>
@@ -54,20 +43,15 @@ import {
   IonContent,
   IonHeader,
   IonToolbar,
-  IonTitle,
   IonText,
   IonBackButton,
   IonButton,
   IonFooter,
-  IonItemGroup,
-  IonItem,
-  IonInput,
-  IonLabel,
-  alertController,
+  IonChip
 } from "@ionic/vue";
 import { useStore } from "vuex";
 import { useRouter } from "vue-router";
-import { DeriveWallet } from "@/lib/wallet";
+import { validateMnemonic } from "bip39";
 
 export default defineComponent({
   name: "NewSeed",
@@ -76,78 +60,63 @@ export default defineComponent({
     IonContent,
     IonHeader,
     IonToolbar,
-    IonTitle,
     IonText,
     IonBackButton,
     IonButton,
     IonFooter,
-    IonItemGroup,
-    IonItem,
-    IonInput,
-    IonLabel,
+    IonChip
   },
   data() {
     return {
-      firstWordNumber: Math.floor(Math.random() * 23) + 1,
-      secondWordNumber: Math.floor(Math.random() * 23) + 1,
-      thirdWordNumber: Math.floor(Math.random() * 23) + 1,
-
-      firstWord: "",
-      secondWord: "",
-      thirdWord: "",
+      assembledMnemonic: [] as string[],
+      splitMnemonic: [] as string[]
     };
   },
   methods: {
-    ordinal: function (n: any) {
-      const s = ["th", "st", "nd", "rd"],
-        v = n % 100;
-      return n + (s[(v - 20) % 10] || s[v] || s[0]);
+    // Adding a word from shuffled to assembled
+    addWord(word: string) {
+      // Determine word index in array and remove it
+      const index = this.splitMnemonic.indexOf(word);
+      this.splitMnemonic.splice(index, 1);
+
+      // Append the word to the assembled array
+      this.assembledMnemonic.push(word);
     },
-    async verify() {
-      const mnemonic = this.store.getters.getWalletMnemonic;
-      const splitMnemonic = mnemonic.split(" ");
 
-      const alert = await alertController.create({
-        header: "Verification failed!",
-        message: "Please check that you inputted the seed words correctly.",
-        buttons: ["Okay"],
-      });
+    // Removing a word from assembled and back into shuffled
+    removeWord(word: string) {
+      // Determine word index and remove it from assembled array
+      const index = this.assembledMnemonic.indexOf(word);
+      this.assembledMnemonic.splice(index, 1);
 
-      if (!splitMnemonic.includes(this.firstWord)) {
-        return alert.present();
-      }
-
-      if (!splitMnemonic.includes(this.secondWord)) {
-        return alert.present();
-      }
-
-      if (!splitMnemonic.includes(this.thirdWord)) {
-        return alert.present();
-      }
-
-      // If all is valid, then move on...
-      const congratulationsAlert = await alertController.create({
-        header: "Congratulations! 🥳",
-        message: "You have successfully verified your 24-word mnemonic!",
-        buttons: [
-          {
-            text: "Okay",
-            handler: async () => {
-              // Generate a wallet with Feirm as a coin
-              await DeriveWallet(this.store.getters.getWalletMnemonic, "xfe").then(wallet => {
-                // Save our wallet
-                this.store.commit("setWalletState", wallet);
-
-                // Navigate to wallets page
-                this.router.push({ path: "/tabs/wallet" });
-              });
-            },
-          },
-        ],
-      });
-
-      return congratulationsAlert.present();
+      // Append the word to the shuffled array
+      this.splitMnemonic.push(word);
     },
+
+    // Verify the assembled mnemonic and move onto the next stage
+    verify() {
+      const mnemonic = this.store.getters.getWalletMnemonic; // Mnemonic generated for us
+      const assembledMnemonic = this.assembledMnemonic.join(" "); // Assembled mnemonic in its sentence form
+
+      // Check the assembled string is valid according to bip39
+      const valid = validateMnemonic(assembledMnemonic);
+      if (!valid) {
+        // TODO Throw some error about validation
+        return;
+      }
+
+      // Check the two mnemonics match each other
+      if (mnemonic === assembledMnemonic) {
+        // TODO Show a success message
+        console.log("The mnemonics match...")
+      }
+    }
+  },
+  created() {
+    // Split and shuffle the array
+    let mnemonicArray = this.store.getters.getWalletMnemonic.split(" ");
+    mnemonicArray = mnemonicArray.sort(() => Math.random() - 0.5);
+    this.splitMnemonic = mnemonicArray;
   },
   setup() {
     const store = useStore();
@@ -160,3 +129,11 @@ export default defineComponent({
   },
 });
 </script>
+
+<style scoped>
+.mnemonic-area {
+  background-color: #eee;
+  padding: 15px;
+  min-height: 4.5em;
+}
+</style>
