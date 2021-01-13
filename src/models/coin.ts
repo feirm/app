f02a76a216ce5fe51d4feaffc2c983de73573798
Jack Coble <jack@coble.uk>
2021-01-13 15:46:09 +0000

    Change balance data types into strings (for satoshis)

--- src/models/coin.ts
+++ src/models/coin.ts
@@ -4,8 +4,8 @@ This interface represents what we want our coin object to look like
 export interface Coin {
     name: string;
     ticker: string;
-    balance: number; // Balance of the coin in Satoshis
-    unconfirmedBalance: number; // Unconfirmed balance of the coin in Satoshis
+    balance: string; // Balance of the coin in Satoshis
+    unconfirmedBalance: string; // Unconfirmed balance of the coin in Satoshis
     rootKey: string; // BIP32 root/master key
     extendedPublicKey: string; // BIP32/44/49 extended public key
     extendedPrivateKey: string; // BIP32/44/49 extended private key
