9430b556a15a6e75d2b822eef3c2317f9561c9d5
Jack Coble <jack@coble.uk>
2021-01-13 13:55:26 +0000

    Initialise wallet on launch and create some getters

--- src/store/modules/wallet/index.ts
+++ src/store/modules/wallet/index.ts
@@ -1,3 +1,6 @@
+import hdWalletP2pkh from "@/class/wallets/hd-wallet-p2pkh";
+import { store } from "@/store";
+
 export const wallet = {
   state: {
     wallet: {},
@@ -6,6 +9,25 @@ export const wallet = {
   mutations: {
     setWalletState(state, wallet) {
       state.wallet = wallet;
+    }
+  },
+  actions: {
+    async initialize({ commit }) {
+      const wallet = await hdWalletP2pkh.loadFromDisk();
+
+      if (wallet) {
+        commit("setWalletState", wallet);
+      }
+    }
+  },
+  getters: {
+    walletState: state => state.wallet,
+    walletExists: state => {
+      if (state.wallet.coins) {
+        return true;
+      }
+
+      return false;
     }
   }
 };
