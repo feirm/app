cb5aaccd7b965bdc0314c9ccfed18f47bc531ff4
Jack Coble <jack@coble.uk>
2021-01-13 17:13:42 +0000

    Format and update the network data before being committed to Vuex

--- src/store/modules/coins/index.ts
+++ src/store/modules/coins/index.ts
@@ -14,6 +14,24 @@ export const coins = {
       // Fetch all coins data
       async setCoins(context) {
           const coins = await azureService.getCoins()
+
+          // Need to iterate over each of the coin data
+          for (let i = 0; i < coins.data.length; i++) {
+            // Current coin details
+            const coin = coins.data[i];
+
+            // Modify the network information
+            // P2PKH
+            if (coin.networks.p2pkh) {
+              coin.networks.p2pkh.pubKeyHash = coin.networks.p2pkh.pubKeyHash[0];
+              coin.networks.p2pkh.scriptHash = coin.networks.p2pkh.scriptHash[0];
+              coin.networks.p2pkh.wif = coin.networks.p2pkh.wif[0];
+
+              // Update the original data
+              coins.data[i].networks.p2pkh = coin.networks.p2pkh;
+            }
+          }
+
           context.commit('setCoins', coins.data);
       }
   },
