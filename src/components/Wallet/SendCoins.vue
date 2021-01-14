b718e623f4a1958c555d03ea24c30513e6847482
Jack Coble <jack@coble.uk>
2021-01-14 18:28:45 +0000

    Begin re-implementation of Send Coins component

--- src/components/Wallet/SendCoins.vue
+++ src/components/Wallet/SendCoins.vue
@@ -1,41 +1,33 @@
 <template>
-  <ion-header>
-    <ion-toolbar class="ion-text-center">
-      <ion-buttons slot="start">
+  <ion-header class="ion-no-border">
+    <ion-toolbar class="ion-text-left">
+      <ion-title>Send {{ this.$props.ticker.toUpperCase() }}</ion-title>
+      <ion-buttons slot="end">
         <ion-button @click="closeModal">
           <ion-icon slot="icon-only" :icon="closeOutline"></ion-icon>
         </ion-button>
       </ion-buttons>
-      <ion-buttons slot="secondary">
-        <ion-button @click="presentAlert">
-          <ion-icon
-            slot="icon-only"
-            :icon="informationCircleOutline"
-          ></ion-icon>
-        </ion-button>
-      </ion-buttons>
-      <ion-title>Send {{ this.$props.ticker.toUpperCase() }}</ion-title>
     </ion-toolbar>
   </ion-header>
   <ion-content class="ion-padding ion-text-center">
-    <ion-item>
-      <ion-label position="stacked">To</ion-label>
-      <ion-input
-        v-model="toAddress"
-        v-on:ionChange="validateAddress($event.target.value)"
-      ></ion-input>
-    </ion-item>
+    <!-- Amount input -->
     <ion-item>
       <ion-label position="stacked">Amount</ion-label>
       <ion-input v-model="amount" type="number" step="0.001"></ion-input>
     </ion-item>
+
+    <!-- Address input -->
     <ion-item>
-      <ion-note>
-        <p @click="useMaxBalance">
-          Spend maximum balance: {{ max }}
-          {{ this.$props.ticker.toUpperCase() }}
-        </p>
-      </ion-note>
+      <ion-input
+        placeholder="Address"
+        v-model="toAddress"
+        v-on:ionChange="validateAddress($event.target.value)"
+      ></ion-input>
+
+      <ion-button color="light" @click="scanQr">
+        <ion-icon slot="start" :icon="scanOutline"></ion-icon>
+        Scan
+      </ion-button>
     </ion-item>
 
     <!-- Send buttons -->
@@ -43,13 +35,6 @@
       Send
       <ion-icon slot="end" :icon="sendSharp"></ion-icon>
     </ion-button>
-
-    <!-- QR Scan button -->
-    <ion-fab slot="fixed" vertical="bottom" horizontal="center">
-      <ion-fab-button @click="scanQr">
-        <ion-icon :icon="qrCodeOutline"></ion-icon>
-      </ion-fab-button>
-    </ion-fab>
   </ion-content>
 </template>
 
@@ -61,20 +46,15 @@ import {
   IonLabel,
   IonInput,
   IonButton,
-  IonNote,
   IonIcon,
-  IonFab,
-  IonFabButton,
   modalController,
-  loadingController,
   alertController,
 } from "@ionic/vue";
-import { Coin, CreateSignedTransaction, FindWallet } from "@/lib/wallet";
+import { Coin, CreateSignedTransaction } from "@/lib/wallet";
 import ScanQR from "@/components/Wallet/ScanQR.vue";
-import { sendSharp, qrCodeOutline, closeOutline } from "ionicons/icons";
-import axios from "axios";
-import BigNumber from "bignumber.js";
+import { sendSharp, qrCodeOutline, closeOutline, scanOutline } from "ionicons/icons";
 import bip21 from "bip21";
+import hdWalletP2pkh from "@/class/wallets/hd-wallet-p2pkh";
 
 export default defineComponent({
   name: "SendCoins",
@@ -92,6 +72,10 @@ export default defineComponent({
       sendDisabled: true,
     };
   },
+  async mounted() {
+    const utxos = await hdWalletP2pkh.getUtxos(this.$props.ticker!, "200000000");
+    console.log(utxos);
+  },
   methods: {
     async validateAddress(address: string) {
       // Check that something is present
@@ -101,123 +85,11 @@ export default defineComponent({
         this.sendDisabled = true;
       }
     },
-    useMaxBalance() {
-      this.amount = this.max;
-    },
     async closeModal() {
       await modalController.dismiss();
     },
     async sendCoins() {
-      const confirm = await alertController.create({
-        header: "Confirm",
-        message: `Are you sure you want to continue?
-        <br />
-        <br />
-        Address: <b>${this.toAddress}</b>
-        <br />
-        Amount: <b>${this.amount} ${this.$props.ticker?.toUpperCase()}</b>
-        <br />
-        TX Fee: <b>${this.fee} ${this.$props.ticker?.toUpperCase()}</b>
-        <br />
-        Total: <b>${new BigNumber(this.amount)
-          .plus(this.fee)
-          .toNumber()} ${this.$props.ticker?.toUpperCase()}</b>`,
-        buttons: [
-          {
-            text: "Cancel",
-          },
-          {
-            text: "Confirm",
-            handler: async () => {
-              // Create a new loading popup
-              await loadingController
-                .create({
-                  message: "Sending coins...",
-                })
-                .then((a) => {
-                  a.present()
-                    .then(async () => {
-                      // Create signed transaction
-                      const hash = await CreateSignedTransaction(
-                        this.$props.ticker as string,
-                        this.toAddress,
-                        this.amount,
-                        this.fee
-                      );
-
-                      // Once complete, dismiss the loading controller
-                      a.dismiss();
-
-                      // Show success alert
-                      const alert = await alertController.create({
-                        header: "Transaction Complete!",
-                        message:
-                          "The transaction was successful! (A fancy page will show here eventually).",
-                        buttons: [
-                          {
-                            text: "View on explorer",
-                            handler: async () => {
-                              const explorer = this.coinObj.blockbook;
-                              const url = explorer + "/tx/" + hash;
-
-                              window.open(url);
-                            },
-                          },
-                          {
-                            text: "Close",
-                            handler: async () => {
-                              // Dismiss the send coins modal
-                              await modalController.dismiss();
-                            },
-                          },
-                        ],
-                      });
-                      return alert.present();
-                    })
-                    .catch(async (e) => {
-                      a.dismiss();
-
-                      // Determine error message to be shown
-                      let errorMessage = "" as any;
-                      errorMessage = e;
-
-                      /*
-                      if (e.response.data.length) {
-                        switch (e.response.data.error) {
-                          // TX Fee is not high enough
-                          case "-26: 66: insufficient priority":
-                            errorMessage =
-                              "Transaction fee is not high enough!";
-                            break;
-                          // Dust (TX is too small)
-                          case "-26: 64: dust":
-                            errorMessage =
-                              "Transaction amount is too small (dust)!";
-                            break;
-                          default:
-                            // Condition isn't met, just use error from HTTP request
-                            errorMessage = "Something unexpected occurred";
-                            break;
-                        }
-                      }
-                      */
-
-                      // Show error alert
-                      const alert = await alertController.create({
-                        header: "Transaction Error",
-                        message: errorMessage,
-                        buttons: ["Close"],
-                      });
-
-                      return alert.present();
-                    });
-                });
-            },
-          },
-        ],
-      });
-
-      return confirm.present();
+      console.log("Send coins....")
     },
     async scanQr() {
       const modal = await modalController.create({
@@ -256,60 +128,13 @@ export default defineComponent({
       }
     },
   },
-  async mounted() {
-    // Loading popup saying "fetching data"
-    await loadingController
-      .create({
-        message: "Fetching data...",
-      })
-      .then((a) => {
-        a.present()
-          .then(async () => {
-            // Find coin based on ticker and set coin data
-            await FindWallet(this.$props.ticker!).then((coin) => {
-              this.coinObj = coin as Coin;
-            });
-
-            // Fetch ideal transaction fee for transaction to be confirmed
-            // within 25 blocks.
-            const txFee = await axios.get(
-              "https://cors-anywhere.feirm.com/" +
-                this.coinObj.blockbook +
-                "/api/v2/estimatefee/25"
-            );
-
-            // Convert the TX fee into satoshi values
-            const fee = new BigNumber(txFee.data.result);
-            this.fee = fee.toNumber();
-
-            // Work out the maximum the user can send taking into consideration the TX fee.
-            const max = new BigNumber(this.coinObj.balance).dividedBy(100000000).minus(fee).minus(fee);
-            this.max = max.toNumber();
-
-            // Dismiss
-            a.dismiss();
-          })
-          .catch(async (e) => {
-            // Dismiss
-            a.dismiss();
-
-            // Show alert
-            const errorAlert = await alertController.create({
-              header: "Error fetching data!",
-              message: e,
-              buttons: ["Close"],
-            });
-
-            return errorAlert.present();
-          });
-      });
-  },
   setup() {
     return {
       CreateSignedTransaction,
       sendSharp,
       qrCodeOutline,
       closeOutline,
+      scanOutline
     };
   },
   components: {
@@ -318,10 +143,7 @@ export default defineComponent({
     IonLabel,
     IonInput,
     IonButton,
-    IonNote,
     IonIcon,
-    IonFab,
-    IonFabButton,
   },
 });
 </script>
