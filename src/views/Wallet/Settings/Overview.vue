9f357dccaae8e36a06fb23cd057baa2b80b21cad
Jack Coble <jack@coble.uk>
2021-02-02 15:33:03 +0000

    Add options to settings overview page

--- src/views/Wallet/Settings/Overview.vue
+++ src/views/Wallet/Settings/Overview.vue
@@ -1,23 +1,56 @@
 <template>
-    <ion-page>
-        <ion-content class="ion-padding" :fullscreen="true">
-            Settings page
-        </ion-content>
-    </ion-page>
+  <ion-page>
+    <ion-header class="ion-no-border">
+      <ion-toolbar class="ion-text-left">
+        <ion-title color="dark">Settings</ion-title>
+        <ion-buttons slot="start">
+          <ion-back-button></ion-back-button>
+        </ion-buttons>
+      </ion-toolbar>
+    </ion-header>
+    <ion-content class="ion-padding" :fullscreen="true">
+      <ion-text>
+          Manage your cryptocurrency wallet. Please be sure you have a backup of your 24-word mnemonic phrase just in case something goes wrong here!
+      </ion-text>
+
+      <hr>
+      <ion-button expand="block">Export / Backup</ion-button>
+      <ion-button expand="block">Show XPUB</ion-button>
+    </ion-content>
+    <ion-footer class="ion-no-border ion-padding ion-text-center">
+      <ion-button color="danger" fill="clear">Remove Coin</ion-button>
+    </ion-footer>
+  </ion-page>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
 import {
-    IonPage,
-    IonContent
+  IonPage,
+  IonContent,
+  IonText,
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonButtons,
+  IonButton,
+  IonBackButton,
+  IonFooter
 } from "@ionic/vue";
 
 export default defineComponent({
-    name: "SettingsOverview",
-    components: {
-        IonPage,
-        IonContent
-    }
-})
+  name: "SettingsOverview",
+  components: {
+    IonPage,
+    IonContent,
+    IonText,
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonButtons,
+    IonButton,
+    IonBackButton,
+    IonFooter
+  },
+});
 </script>
