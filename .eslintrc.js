fd5d74085992d05c6559aee18bb9798ee0b7b751
Jack Coble <jack@coble.uk>
2021-02-03 20:20:59 +0000

    Fix ESlint configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,7 +16,8 @@ module.exports = {
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'vue/no-deprecated-slot-attribute': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
-    '@typescript-eslint/no-this-alias': 'off'
+    '@typescript-eslint/no-this-alias': 'off',
+    '@typescript-eslint/camelcase': 'on'
   },
   overrides: [
     {
