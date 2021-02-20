7c7b7b915e15dd80da663ef85027bb30ee298931
Jack Coble <jack@coble.uk>
2021-02-20 17:56:04 +0000

    Start of about/version page

---
+++ src/views/Settings/Version.vue
@@ -0,0 +1,39 @@
+<template>
+  <ion-page>
+    <ion-header class="ion-no-border">
+      <ion-toolbar class="ion-text-left">
+        <ion-buttons slot="start">
+          <ion-back-button></ion-back-button>
+        </ion-buttons>
+        <ion-title color="dark">Version</ion-title>
+      </ion-toolbar>
+    </ion-header>
+    <ion-content fullscreen="true" class="ion-padding"> </ion-content>
+  </ion-page>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import {
+    IonPage,
+    IonContent,
+    IonHeader,
+    IonToolbar,
+    IonButtons,
+    IonBackButton,
+    IonTitle
+} from "@ionic/vue";
+
+export default defineComponent({
+  name: "Version",
+  components: {
+    IonPage,
+    IonContent,
+    IonHeader,
+    IonToolbar,
+    IonButtons,
+    IonBackButton,
+    IonTitle
+  },
+});
+</script>
