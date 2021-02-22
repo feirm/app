88b5c0fa7fe33378f279c7e591f4f0d64b5c681c
Jack Coble <jack@coble.uk>
2021-02-22 19:15:22 +0000

    Prompt user when there is a new update available

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <ion-app>
     <ion-router-outlet />
+    <button @click="refreshApp" v-if="updateExists">New version available. Click to update!</button>
   </ion-app>
 </template>
 
@@ -13,6 +14,43 @@ export default defineComponent({
   components: {
     IonApp,
     IonRouterOutlet
+  },
+  data() {
+    return {
+      refreshing: false,
+      registration: null as any,
+      updateExists: false
+    }
+  },
+  created() {
+    document.addEventListener('swUpdated', this.showRefreshUI, { once: true })
+
+    if (navigator.serviceWorker) {
+      navigator.serviceWorker.addEventListener(
+        'controllerchange', () => {
+          if (this.refreshing) {
+            return;
+          }
+
+          window.location.reload();
+        }
+      )
+    }
+  },
+  methods: {
+    showRefreshUI(e) {
+      this.registration = e.detail;
+      this.updateExists = true;
+    },
+    refreshApp() {
+      this.updateExists = false;
+
+      if (!this.registration || !this.registration.waiting) {
+        return;
+      }
+
+      this.registration.waiting.postMessage('skipWaiting');
+    }
   }
 });
 </script>
