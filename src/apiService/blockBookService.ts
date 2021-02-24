9e23285b95cfddaff9573290b32f7331856713b8
Jack Coble <jack@coble.uk>
2021-02-24 13:46:32 +0000

    Remove unused Blockbook service

--- src/apiService/blockBookService.ts
+++
@@ -1,8 +0,0 @@
-import { blockBookApi } from "@/apiService/Api";
-
-export default {
-    // Return data for xpub
-    getXpub(xpub: string) {
-        return blockBookApi.get(`/v2/xpub/${xpub}`);
-    }
-}
