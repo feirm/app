4d850544c676427f44341cdac1ca08c5edf2c8ce
Jack Coble <jack@coble.uk>
2021-02-25 09:42:10 +0000

    Update contact model

--- src/models/contact.ts
+++ src/models/contact.ts
@@ -1,5 +1,21 @@
-export interface EncryptedContact {
+// Representation of an encrypted contact stored by the API
+interface EncryptedContact {
   id: string;
   cipherText: string;
   iv: string;
+}
+
+// Contact in its decrypted form
+interface Contact {
+  id: string;
+  firstName: string;
+  lastName: string;
+  phoneNumber: string;
+  emailAddress: string;
+}
+
+// Exports
+export {
+  EncryptedContact,
+  Contact
 }
