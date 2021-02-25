bde201d304b7ad16a2d0615a47a67209f5553a5e
Jack Coble <jack@coble.uk>
2021-02-25 09:16:40 +0000

    Methods to delete and fetch all encrypted contacts from IndexedDB

--- src/class/contacts.ts
+++ src/class/contacts.ts
@@ -7,9 +7,24 @@ class Contacts extends DB {
         super();
     }
 
+    // Add encrypted contact
     addContact(contact: EncryptedContact) {
         this.contacts.add(contact, contact.id);
-    }    
+    }
+
+    // Delete encrypted contact
+    deleteContact(contactId: string) {
+        this.contacts.delete(contactId);
+    }
+
+    // Fetch all encrypted contacts
+    getEncryptedContacts(): EncryptedContact[] {
+        const contacts: EncryptedContact[] = [];
+
+        this.contacts.each(contact => contacts.push(contact));
+
+        return contacts;
+    }
 }
 
 export default new Contacts();
