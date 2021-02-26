5886cc8365b1fe7fd52645ccd6fc53f23b1f1c51
Jack Coble <jack@coble.uk>
2021-02-26 13:47:54 +0000

    Update account model

--- src/models/account.ts
+++ src/models/account.ts
@@ -1,15 +1,11 @@
 interface Account {
+  id: string;
   username: string;
-  pin?: string;
   rootPasswordSalt: string;
   rootPublicKey: string;
   encryptedRootKey: {
     cipherText: string;
     iv: string;
-  };
-  token?: {
-    id: string;
-    signature: string;
   };
 }
 
