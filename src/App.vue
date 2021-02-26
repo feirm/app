82954ea37c716a3ff57587129e7456d495c932a3
Jack Coble <jack@coble.uk>
2021-02-26 22:33:07 +0000

    Prompt for account decryption if root key is not present in memory

--- src/App.vue
+++ src/App.vue
@@ -5,14 +5,39 @@
 </template>
 
 <script lang="ts">
-import { IonApp, IonRouterOutlet } from '@ionic/vue';
-import { defineComponent } from 'vue';
+import { IonApp, IonRouterOutlet, modalController } from "@ionic/vue";
+import { defineComponent, onBeforeMount } from "vue";
+
+import PasswordPrompt from "@/components/Auth/Password.vue";
+import Account from "@/class/account";
 
 export default defineComponent({
-  name: 'App',
+  name: "App",
   components: {
     IonApp,
-    IonRouterOutlet
-  }
+    IonRouterOutlet,
+  },
+  async beforeCreate() {
+    // When the app is mounting, prompt for password entry to decrypt
+    // Check if account root key is already in cache
+    if (Account.getRootKey() !== undefined) {
+      return;
+    }
+
+    // Check if an encrypted account is present
+    const username = localStorage.getItem("username")!;
+    const account = await Account.fetchAccountFromIDB(username);
+    if (!account) {
+      // Do not want to continue if they don't have an account
+      return;
+    }
+
+    // We have the encrypted account, so we can request for a password
+    const passwordPrompt = await modalController.create({
+      component: PasswordPrompt,
+    });
+
+    passwordPrompt.present();
+  },
 });
 </script>
