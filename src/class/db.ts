bc410f845d64e804d1422708546e78b4963798f4
Jack Coble <jack@coble.uk>
2021-02-26 13:49:05 +0000

    Add accounts table to DB class

--- src/class/db.ts
+++ src/class/db.ts
@@ -1,19 +1,24 @@
 // This DB class will be a subclass of Dexie - the IndexedDB for data storage
-import { EncryptedContact } from "@/models/contact";
 import Dexie from "dexie";
+
+import { Account } from "@/models/account";
+import { EncryptedContact } from "@/models/contact";
 
 class DB extends Dexie {
   // Tables
-  contacts: Dexie.Table<EncryptedContact>
+  account: Dexie.Table<Account>;
+  contacts: Dexie.Table<EncryptedContact>;
 
   // Initialise the constructor
   constructor() {
     super("feirm");
     this.version(1).stores({
+      account: 'id, username',
       contacts: 'id, cipherText, iv'
     })
 
     // Assign our tables
+    this.account = this.table("account");
     this.contacts = this.table("contacts");
   }
 }
