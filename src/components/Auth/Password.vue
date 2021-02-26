3e1a5b1986d5fdc8b0a62bb1b3ad3f0db1703d6f
Jack Coble <jack@coble.uk>
2021-02-26 17:25:07 +0000

    Start of the password entry component

---
+++ src/components/Auth/Password.vue
@@ -0,0 +1,13 @@
+<template>
+    <ion-content :fullscreen="true" class="ion-padding">
+
+    </ion-content>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+    name: "Password",
+})
+</script>
