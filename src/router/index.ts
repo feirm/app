024e91932cb547d0155ef968f09019ebe4347f86
Jack Coble <jack@coble.uk>
2021-02-26 21:23:19 +0000

    Remove old authentication code from router

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,10 +1,6 @@
 import { createRouter, createWebHistory } from "@ionic/vue-router";
 import { RouteRecordRaw } from "vue-router";
-import { store } from "@/store";
 import Tabs from "../views/Tabs.vue";
-import tatsuyaService from "@/apiService/tatsuyaService";
-import * as nacl from "tweetnacl";
-import bufferToHex from "@/lib/bufferToHex";
 
 const routes: Array<RouteRecordRaw> = [
   // Redirect index page to Discover route
@@ -112,73 +108,6 @@ const router = createRouter({
   routes,
 });
 
-// Check if the user is already logged in.
-router.beforeEach(async (to, from, next) => {
-  const loggedIn = store.getters.isUserLoggedIn;
-  const authRequired = to.matched.some((route) => route.meta.requiresAuth);
-
-  // If the user isn't logged in, but has a root key, get a new session token
-  const rootKey = store.getters.getRootKey;
-  const username = store.getters.getUsername;
-
-  if (!loggedIn && rootKey) {
-    // Reconstruct the identity key from the root key
-    const identityKeyString = rootKey + "identity";
-    const identityKey = await window.crypto.subtle.digest(
-      "SHA-256",
-      new TextEncoder().encode(identityKeyString)
-    );
-
-    // Derive the signing ed25519 keypair from identityKey
-    const rootKeyPair = nacl.sign.keyPair.fromSeed(new Uint8Array(identityKey));
-
-    await tatsuyaService
-      .getLoginToken(username)
-      .then(async (res) => {
-        // Create signature
-        const signature = nacl.sign.detached(
-          new TextEncoder().encode(res.data.nonce),
-          rootKeyPair.secretKey
-        );
-
-        // Construct the login/new session payload
-        const payload = {
-          id: res.data.id,
-          username: username,
-          signature: bufferToHex(signature),
-        };
-
-        // Submit the signed payload and expect to receive a new session token
-        await tatsuyaService.loginAccount(payload).then((res) => {
-          // Construct the session object
-          const session = {
-            rootKey: rootKey,
-            sessionToken: res.data.sessionToken,
-            username: res.data.username,
-          };
-
-          // Save the session token
-          store.dispatch("login", session);
-
-          // Check to see if authed, and if so, continue
-          if (store.getters.isUserLoggedIn) {
-            next();
-          }
-        });
-      })
-      .catch((e) => {
-        if (e.status === 400) {
-          // User might not exist (maybe database reset), so return them to login page
-          return next("/");
-        }
-      });
-  }
-
-  if (authRequired && !loggedIn) {
-    next("/auth/login");
-  } else {
-    next();
-  }
-});
+// TODO: Check that the user's root key is present in session storage
 
 export default router;
