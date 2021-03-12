1aec2218f11bd59a528c01efffeb71b33b6612d1
Jack Coble <jack@coble.uk>
2021-03-12 20:41:01 +0000

    Update paramter types for authService

--- src/apiService/authService.ts
+++ src/apiService/authService.ts
@@ -1,9 +1,9 @@
-import { EncryptedAccountV2 } from "@/models/account"
+import { EncryptedKey } from "@/models/account"
 import { keyStorageApi } from "./Api"
 
 export default {
     // Submit a key to the new authentication API
-    SendKey(account: EncryptedAccountV2) {
-        return keyStorageApi.post("/v1/add_key", account)
+    SendKey(key: EncryptedKey) {
+        return keyStorageApi.post("/v1/add_key", key)
     }
 }
