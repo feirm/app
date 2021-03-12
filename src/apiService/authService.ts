735daaa5e48fbe78e8f3d143e9dc473cc712c500
Jack Coble <jack@coble.uk>
2021-03-12 20:10:10 +0000

    Typed parameters for authService

--- src/apiService/authService.ts
+++ src/apiService/authService.ts
@@ -1,8 +1,9 @@
+import { EncryptedAccountV2 } from "@/models/account"
 import { keyStorageApi } from "./Api"
 
 export default {
     // Submit a key to the new authentication API
-    SendKey(keyPayload) {
-        return keyStorageApi.post("/v1/add_key", keyPayload)
+    SendKey(account: EncryptedAccountV2) {
+        return keyStorageApi.post("/v1/add_key", account)
     }
 }
